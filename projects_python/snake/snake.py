600eb4847ace7af6e222a8b0a6ebb4352df51023
10x_devToBe <tanguy.mauve@proton.me>
2022-09-24 22:14:36 +0200

    edit snake.py

--- projects_python/snake/snake.py
+++ projects_python/snake/snake.py
@@ -20,36 +20,39 @@ def deplacement():
     deplacement = input("direction :")
     if deplacement == "d":
         #x go right
-        x =+ 1
+        x += 1
         print(x)
     elif deplacement == "q":
         #x go left
-        x =- 1
+        x -= 1
         print(x)
     elif deplacement == "z":
         #x go up
-        y =+ 1
+        y += 1
         print(y)
     elif deplacement == "s":
         #x go down
-        y =- 1
+        y -= 1
         print(y)
-
         
-
-if  i == y:
-   print("|", end='')
-   for i in range(x):
-     print(' ', end='')
-   print('X', end='')
-   for i in range(8 - x):
-     print(' ', end='')
-   print("|", end='')
+#print("|", end='')
+#for i in range(x):
+#print(' ', end='')
+# print(snake, end='')
+#for i in range(8 - x):
+#print(' ', end='')
+#print("|", end='')
 
 i = 1
+c = 1
 while i != 0:
     deplacement()
     print(" ---------")
-    for i  in range(8):
-        print("|         |")
+    for c in range(8):
+        print("|")
+    for c in range(x):
+            #print("|", end="")
+        print(x*"", end="")
+        print(y*"", end="")
+        print(snake, end="")
     print(" ---------")
