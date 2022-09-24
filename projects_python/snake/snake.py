155dc8011be6ba653460d20428b7410a6d402fb5
10x_devToBe <tanguy.mauve@proton.me>
2022-09-24 17:14:07 +0200

    add snake.py

---
+++ projects_python/snake/snake.py
@@ -0,0 +1,21 @@
+
+#créer le terrain de jeu, avec des limites à ne pas dépasser
+#créer un serpent
+#lui donner 4 mouvements : haut bas gauche droite décidé par l'utilisateur
+#faire en sorte que le serpent puisse tout le temps bouger
+#lui donner une pomme à manger qui peut apparaître partout
+snake = x
+apple = o
+print("hello and welcome to snake ! d is for going right, q going left, z going up and s going down !")
+
+def deplacement():
+    deplacement = input("direction :")
+    if deplacement == "d":
+        #x go right
+    elif deplacement == "q":
+        #x go left
+    elif deplacement == "z":
+        #x go up
+    elif deplacement == "s":
+        #x go down
+
