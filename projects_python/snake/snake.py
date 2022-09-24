accece9e7016c8370c8ae5412c04d50cc6294189
10x_devToBe <tanguy.mauve@proton.me>
2022-09-24 18:26:57 +0200

    edit snake.py

--- projects_python/snake/snake.py
+++ projects_python/snake/snake.py
@@ -4,18 +4,28 @@
 #lui donner 4 mouvements : haut bas gauche droite décidé par l'utilisateur
 #faire en sorte que le serpent puisse tout le temps bouger
 #lui donner une pomme à manger qui peut apparaître partout
-snake = x
-apple = o
+snake = "x"
+apple = "o"
 print("hello and welcome to snake ! d is for going right, q going left, z going up and s going down !")
-
+print(" ---------")
+for i  in range(8):
+    print("|         |")
+print(" ---------")
 def deplacement():
     deplacement = input("direction :")
     if deplacement == "d":
         #x go right
+        print("d")
     elif deplacement == "q":
         #x go left
+        print("q")
     elif deplacement == "z":
         #x go up
+        print("z")
     elif deplacement == "s":
         #x go down
+        print("s")
 
+
+
+deplacement()
