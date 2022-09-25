9f8a692f6f13b988cace7afa084ef78386186d5f
10x_devToBe <tanguy.mauve@proton.me>
2022-09-26 01:05:51 +0200

    edit snake.py, at this point snake move up down right left

--- projects_python/snake/snake.py
+++ projects_python/snake/snake.py
@@ -24,42 +24,20 @@ def deplacement():
         print(x)
     elif deplacement == "z":
         #x go up
-        y += 1
+        y -= 1
         print(y)
     elif deplacement == "s":
         #x go down
-        y -= 1
+        y += 1
         print(y)
 
 i = 0
-while i !=9: #permet de faire une boucle demandant l'input
+while True: #permet de faire une boucle demandant l'input
     deplacement() #appel la fonction pour les déplacements du snake
     #while i !=y:
      #   print("|        |")
-    for i in range(8 - y):      
+    for i in range(8):      
         if  i == y:
-            print("|", (x-1)*"", snake, (8 - x)*"", "|",)
+            print("|" + (x)*" " + snake + (8 - x -1)*" " + "|")
         else:
-            print("|        |") 
-        #for i in range(8 - y):
-         #   print("|        |")
-        #for i in range(8 - x):
-        #    print(' ', end='')
-        #    print("|", end='')
-
-"""
-i = 1
-while i != 0: #permet de faire une boucle demandant l'input 
-    deplacement() #appel la fonction pour les déplacements du snake
-    for c in range(8): #initialise la barre du haut
-        print("-", end="") 
-    for c in range(8): #imprime les barres sur le côté gauche
-        print("|")
-    for c in range(x): #print le serpent en bas à gauche 
-        print(snake, end="")
-    for c in range(8): #print la barre du bas
-        #print("", "-",end="")
-        print("-", end="")
-    # il faut réussir à garder snake unique
-    # et ensuite à faire en sorte qu'il reste sur le même axe
-"""
+            print("|        |")
