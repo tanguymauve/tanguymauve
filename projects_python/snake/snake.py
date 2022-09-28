41f3e7a53103501932845095efd63a896e86b247
10x_devToBe <tanguy.mauve@proton.me>
2022-09-29 01:54:09 +0200

    edit snake.py

--- projects_python/snake/snake.py
+++ projects_python/snake/snake.py
@@ -16,41 +16,43 @@ print("hello and welcome to snake ! d is for going right, q going left, z going 
 #Fonctions 
 def deplacement():
     deplacement = input("direction :")
-    if deplacement == "d" and playerCoordinateX[-1] <= 6:
+    if deplacement == "d" and playerCoordinateX[-1] < 7:
         #player go right
         playerCoordinateX.append(playerCoordinateX[-1] + 1)
         playerCoordinateY.append(playerCoordinateY[-1])
         print("player_x =", playerCoordinateX[-1], "player_y =", playerCoordinateY[-1])
             
-    elif deplacement == "q" and playerCoordinateX[-1] <= 6:
+    elif deplacement == "q" and playerCoordinateX[-1] > 0:
         #player go left
         playerCoordinateX.append(playerCoordinateX[-1] - 1)
         playerCoordinateY.append(playerCoordinateY[-1])
         print("player_x =", playerCoordinateX[-1], "player_y =", playerCoordinateY[-1])  
         
-    elif deplacement == "z" and playerCoordinateY[-1] <= 6:
+    elif deplacement == "z" and playerCoordinateY[-1] > 0:
         #player_x go up
         playerCoordinateX.append(playerCoordinateX[-1])
         playerCoordinateY.append(playerCoordinateY[-1] - 1)
         print("player_x =", playerCoordinateX[-1], "player_y =", playerCoordinateY[-1])
         
-    elif deplacement == "s" and playerCoordinateY[-1] <= 6:
+    elif deplacement == "s" and playerCoordinateY[-1] < 7:
         #player_x go down
         playerCoordinateX.append(playerCoordinateX[-1])
         playerCoordinateY.append(playerCoordinateY[-1] + 1)
         print("player_x =", playerCoordinateX[-1], "player_y =", playerCoordinateY[-1])
-        
-    elif playerCoordinateX[-1] == 7 or playerCoordinateY[-1] == 7 or playerCoordinateX[-1] == 0 or playerCoordinateY[-1] == 0:
+    else:
         print("The snake is not thin enough !")
-
+        
 def generateApple():
     global xApple
     global yApple
     xApple = random.randrange(8)
     yApple = random.randrange(8)
+
      
 generateApple()
 while True:
+    if playerCoordinateX[-1] == xApple and playerCoordinateY[-1] == yApple:
+        generateApple()
     deplacement()
     print(" ........")
     for y in range(8):
